46e44d97f06263ad66112c14c8da059640fb732d
James Matthew Dela Vega <117718580+cod3rj@users.noreply.github.com>
2023-11-02 04:05:41 +0800

    Add files via upload

---
+++ student/admin/delete_complaint.php
@@ -0,0 +1,11 @@
+<?php
+	include('config.php');
+
+	$id = $_GET['complaint'];
+	
+	$sql3="delete from complaint where notice_id='$id'";
+	$con->query($sql3);
+	
+
+	header("Location: complaint.php?id=4");
+?>
---
+++ student/admin/edit_complaint.php
@@ -0,0 +1,18 @@
+<?php
+	include('config.php');
+
+	$id=$_GET['complaint'];
+
+	$title=$_POST['title'];
+    $room_building=$_POST['room_building'];
+	$notice=$_POST['notice'];
+	$date=$_POST['date'];
+	
+
+	$sql="update complaint set n_type='$title', room_building = '$room_building', notice='$notice',date='$date' where notice_id='$id'";
+	$con->query($sql);
+	
+	header("Location: complaint.php?id=3");
+	
+	
+?>
